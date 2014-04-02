1bf5c405aeef5898d851c38a58be2dab3b8a9657
Siebrand Mazeland <siebrand@kitano.nl>
2014-04-02 19:11:11 +0200

    Migrate to JSON i18n
    Procedure per https://www.mediawiki.org/wiki/Manual:GenerateJsonI18n.php
    with shim.
    
    Change-Id: Ib94328654861643f98ed1e92f92c36167aed1302

--- TimeMachine.php
+++ TimeMachine.php
@@ -4,11 +4,12 @@ $wgExtensionCredits['specialpage'][] = array(
 	'path' => __FILE__,
 	'name' => 'TimeMachine',
 	'descriptionmsg' => 'timemachine-desc',
-	'version' => 0.1,
+	'version' => '0.2.0',
 	'author' => 'Luis Felipe Schenone',
 	'url' => 'https://www.mediawiki.org/wiki/Extension:TimeMachine',
 );
 
+$wgMessagesDirs['TimeMachine'] = __DIR__ . '/i18n';
 $wgExtensionMessagesFiles['TimeMachine'] = __DIR__ . '/TimeMachine.i18n.php';
 $wgExtensionMessagesFiles['TimeMachineAlias'] = __DIR__ . '/TimeMachine.alias.php';
 

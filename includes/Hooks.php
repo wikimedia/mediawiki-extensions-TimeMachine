33581a9b9d4943951d0b12f7c154ef4c5c857872
Jayprakash12345 <0freerunning@gmail.com>
2021-12-01 20:38:40 +0530

    Refactor the SpecialPage and Hook
    * Bump extension version to 0.4
    * Bump MediaWiki version to 1.31
    * Use AutoloadNamespaces instead AutoloadClasses
    * Remove redundant Revision::newFromId
        call casuing Class 'Revision' not found
    * Separate SpecialPage and Hook to their own file.
    
    Bug: T296705
    Change-Id: I91909f435fc41f79cce701848ec754c97b9b8553
    (cherry picked from commit 022d616af8ca4925e621e492db00cc4734e8342e)

---
+++ includes/Hooks.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace MediaWiki\Extension\TimeMachine;
+
+class Hooks {
+	/**
+	 * This method redirects to the first revision before the time set by the user in Special:TimeMachine
+	 * It would be better if instead of redirecting it changed the request on the fly, but I haven't found
+	 * a way yet.
+	 *
+	 * @param Title &$title
+	 * @param \Article &$article
+	 * @param OutputPage &$output
+	 * @param User &$user
+	 * @param \WebRequest $request
+	 * @param \MediaWiki $mediaWiki
+	 */
+	public static function onBeforeInitialize( &$title, &$article, &$output, &$user, $request, $mediaWiki ) {
+		if ( $request->getVal( 'action', 'view' ) != 'view' ) {
+			return;
+		}
+
+		$date = $request->getCookie( 'timemachine-date' );
+		if ( !$date || $request->getBool( 'oldid' ) ) {
+			return;
+		}
+
+		$dbr = wfGetDB( DB_REPLICA );
+
+		$rev_timestamp = wfTimestamp( TS_UNIX, $date . ' 00:00:00' );
+		$rev_page = $title->getArticleID();
+
+		$rev_id = $dbr->selectField(
+			'revision',
+			'rev_id',
+			[ 'rev_page' => $rev_page, 'rev_timestamp < ' . $dbr->timestamp( $rev_timestamp ) ],
+			__METHOD__,
+			[ 'ORDER BY' => 'rev_timestamp DESC', 'LIMIT' => 1 ]
+		);
+
+		// The page doesn't exist yet
+		if ( !$rev_id ) {
+			return;
+		}
+
+		// Redirect to the old revision of the page
+		$url = $title->getLocalURL( [ 'oldid' => $rev_id ] );
+		$output->redirect( $url );
+	}
+}
